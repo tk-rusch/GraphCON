d58fab941a4f86ea61bf845a487d58df044cc08c
tk-rusch <konstantin.rusch@sam.math.ethz.ch>
2022-06-23 18:25:23 -0700

    changed gcn hps for superpixel

--- src/Superpixels/best_params.py
+++ src/Superpixels/best_params.py
@@ -1,3 +1,3 @@
-best_params_dict = {'GraphCON_GCN': {'lr': 0.00116, 'alpha': 1.396, 'gamma': 0.841, 'nlayers': 10, 'dropout': 0.04},
+best_params_dict = {'GraphCON_GCN': {'lr': 0.00116, 'alpha': 1.396, 'gamma': 0.841, 'nlayers': 10, 'dropout': 0.014151415141514152},
                     'GraphCON_GAT': {'lr': 0.00028, 'alpha': 0.972, 'gamma': 0.348, 'nlayers': 14, 'dropout': 0.04}
                     }
