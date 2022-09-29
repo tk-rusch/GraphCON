3326c48bd3631b9f25417455ded9a1a83ccd3e93
tk-rusch <konstantin.rusch@sam.math.ethz.ch>
2022-09-29 16:39:48 +0200

    fixed typos in heterophilic task

--- src/heterophilic_graphs/models.py
+++ src/heterophilic_graphs/models.py
@@ -61,7 +61,7 @@ class GraphCON_GAT(nn.Module):
     def __init__(self, nfeat, nhid, nclass, nlayers, dropout, dt=1., alpha=1., gamma=1., nheads=4):
         super(GraphCON_GAT, self).__init__()
         self.alpha = alpha
-        self.alpha = gamma
+        self.gamma = gamma
         self.dropout = dropout
         self.nheads = nheads
         self.nhid = nhid
@@ -87,7 +87,7 @@ class GraphCON_GAT(nn.Module):
         Y = F.dropout(Y, self.dropout, training=self.training)
         X = F.dropout(X, self.dropout, training=self.training)
 
-        for i in range(self.N):
+        for i in range(self.nlayers):
             Y = Y + self.dt*(F.elu(self.conv(X, edge_index) + self.res_connection(X)).view(n_nodes, -1, self.nheads).mean(dim=-1) - self.alpha*Y - self.gamma*X)
             X = X + self.dt*Y
             Y = F.dropout(Y, self.dropout, training=self.training)
